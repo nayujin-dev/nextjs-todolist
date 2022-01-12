514dd3177f20ee7117253669d17f69680953bbf3
nayujin-dev <00skdbwls@gmail.com>
2022-01-12 14:02:24 +0900

    delete login

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+//import { getSortedPostsData } from '../components/posts'
 
-  export default function Home() {
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData
+//     }
+//   }
+// }
+
+export default function Home() {
   return (
     <Layout home>
       <Head>
@@ -11,11 +21,9 @@ import utilStyles from '../styles/utils.module.css'
       </Head>
       <section className={utilStyles.headingMd}>
         <Link href="../posts/App">
-          <a>Login First</a>
+          <a>write your schedule</a>
         </Link>
       </section>
     </Layout>
   )
 }
-
-import React from "react";
--- pages/posts/App.js
+++ pages/posts/App.js
@@ -1,8 +1,11 @@
 import React, {useState,useEffect} from "react";
-import AppRouter from "./Router";
+import {Switch} from "react-router-dom";
+//import AppRouter from "./Router";
  import { authService } from "../_app";
-
-function App() {
+ import Home from "./Home";
+ import Auth from "./Auth"
+ 
+function App({isLoggedIn}) {
   const [init,setInit]=useState(false);
   const [userObj,setUserObj]=useState(null);
   useEffect(()=> {
@@ -19,27 +22,44 @@ function App() {
       setInit(true);
     });
   }, []);
-  const refreshUser = () => {
+  const refreshUser=()=>{
     const user = authService.currentUser;
+    const isLoggedIn=false;
     setUserObj({
       displayName: user.displayName,
       uid: user.uid,
       updateProfile: (args) => user.updateProfile(args),
     });
   };
+  // const app= ({ refreshUser, isLoggedIn, userObj }) => {
   return (
     <>
-      {init ? (
-        <AppRouter
-        refreshUser={refreshUser}
-        isLoggedIn={Boolean(userObj)}
-        userObj={userObj}
-      />
+      {init ? 
+      ( <><div>
+        {isLoggedIn=true}
+        {isLoggedIn?(
+           <div
+           style={{
+           maxWidth: 890,
+           width: "100%",
+           margin: "0 auto",
+           marginTop: 80,
+           display: "flex",
+           justifyContent: "center",
+           }}>
+            <Home userObj={userObj} />
+          </div>
+        ):(
+          <Auth />
+        )}
+      </div>
+        </>
       ) : (
         "Initializing..."
       )}
     </>
-  )
-}
+  );
+};
 
 export default App;
+// export default app;
